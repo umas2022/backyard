313dd96a62f8eb6ca1314b466aac1e4c05230459
umas <1970313791@qq.com>
2023-09-02 18:03:25 +0800

    fix

--- hello_world/src/views/PageLoading.vue
+++ hello_world/src/views/PageLoading.vue
@@ -2,6 +2,8 @@
     <!-- 加载页 -->
     <div class="loading">
         <h1>loading ... (还没做)</h1>
+        <h1>看到loading一般不会超过3秒</h1>
+        <h1>↑可能需要切换神秘应用的节点</h1>
     </div>
 </template>
 <script lang="ts" setup>
